b8860ed879735833bf40a6e39b2a2f095747608b
GauravRawt <rawatgaurav793@gmail.com>
2026-05-18 13:04:38 +0530

    first commit

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        for i in range(1,11):
-            a = 10*i
-            self.response.write('10 * {i} = {a} <br>')
+        for i in range(1, 11):
+            self.response.write("10 x {} = {}<br>".format(i, 10 * i))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
